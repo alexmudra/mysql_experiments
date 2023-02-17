a464d035c001c8c6973a694ad5e54e9a63c228d9
alexmudra <ko9#ba9$>
2023-02-17 09:45:34 +0200

    add read csv func via panda module, add csv files, add insert func

---
+++ create_table_in_db.py
@@ -0,0 +1,47 @@
+import mysql.connector
+import click
+
+
+@click.command()
+@click.option('--host', default="localhost", help='MySQL to connect to')
+@click.option('--port', default=3306, help='MySQL to connect to')
+@click.option('--username', help='Database username')
+@click.option('--password', help='Database password')
+@click.option('--database', help='Database to user')
+def create_table(host, port, username, password, database):
+    db_connector = mysql.connector.connect(user=username, password=password, host=host, port=port, database=database)
+    cursor = db_connector.cursor()
+
+#     # Create bar table
+#     cursor.execute("""
+#     CREATE TABLE bar (
+# ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+# NAME VARCHAR(150) NOT NULL,
+# ADDRESS VARCHAR(250) NOT NULL
+# )
+#     """)
+#
+#     # Create item table
+#     cursor.execute("""
+#     CREATE TABLE item (
+# ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+# ITEM_NAME VARCHAR(250) NOT NULL,
+# PRICE FLOAT NOT NULL
+# );
+#     """)
+
+    #Create intermediate table to store one item in multiple tables
+    cursor.execute("""
+    CREATE TABLE item_bar (
+    ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    BAR_ID INT NOT NULL,
+    ITEM_ID INT NOT NULL,
+    FOREIGN KEY (BAR_ID) REFERENCES bar(ID),
+    FOREIGN KEY (ITEM_ID) REFERENCES item(ID)
+    )
+    """)
+
+    db_connector.close()
+
+if __name__ == "__main__":
+    create_table()
---
+++ helper.py
@@ -0,0 +1,19 @@
+import pandas
+
+#read csv via panda module
+def read_csv(path_to_csv):
+    print(tuple(dict(item[1]) for item in tuple(pandas.read_csv(path_to_csv, sep=';', header=0, index_col = None).iterrows())))
+    return tuple(dict(item[1]) for item in tuple(pandas.read_csv(path_to_csv, sep=';', header=0, index_col = None).iterrows()))
+'''
+tuple(dict( - це кортеж діктів
+sep='; - сепаратор яким розділяються колонки в csv-ішках
+header=0 -> хедер 0 не буде парсити 0й рядок в csv, н-д ITEM_NAME;PRICE. Пандас просто сприйме дані як заголовок
+index_col = None - параметр який визначає що жодна колонка не буде індексуватися
+.iterrows - ітератор
+'''
+
+#insert data to db table
+def form_insert_from_dict_tuple(table_name, dict_tuple: [dict])->str:
+    return f"INSERT INTO {table_name}({', '.join(dict_tuple[0].keys())}) VALUES {', '.join([str(tuple(i.values())) for i in dict_tuple])}"
+
+read_csv(shops.csv)
