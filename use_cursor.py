de48c521b58e12621c49fbd3af5b3711ad746b39
alexmudra <ko9#ba9$>
2023-02-17 12:06:37 +0200

    add fetchmany selection func

--- use_cursor.py
+++ use_cursor.py
@@ -18,11 +18,17 @@ cursor.execute(sql)
 result = cursor.fetchone();
 print("fetchone selection result:", result) #fetchone selection result: (1, 'Kolo', 'Kyiv, Sahaidachnogo st 2')
 
+# The fetchmany() method is similar to the fetchone() but, it retrieves the next set of rows in the result set of a query, instead of a single row.
+result = cursor.fetchmany(2);
+print("fetchmany selection result:", result) #fetchmany selection result: [(2, 'ATB', 'Kyiv, Dovbusha st 10'), (3, 'Silpo', 'Teatralna street 45')]
+
 #Fetching 1st row from the table
 result = cursor.fetchall();
 print(f"fetall selection:",result) #fetall selection: [(2, 'ATB', 'Kyiv, Dovbusha st 10'), (3, 'Silpo', 'Teatralna street 45'),
 # (4, 'Kolo', 'Kyiv, Sahaidachnogo st 2'), (5, 'ATB', 'Kyiv, Dovbusha st 10'),
 # (6, 'Silpo', 'Teatralna street 45'), (7, 'Kolo', 'Kyiv, Sahaidachnogo st 2'), (8, 'ATB', 'Kyiv, Dovbusha st 10'), (9, 'Silpo', 'Teatralna street 45')]
+
+
 
 #Closing the connection
 conn.close()
